c2bb13a3a4e14ceeeb5a52d38aa5e6ee1101c629
kitaye <n.s.sadovnikov@mail.ru>
2019-12-09 19:54:24 +0300

    1

--- src/main.js
+++ src/main.js
@@ -2,14 +2,3 @@ import "./styles/main.pcss";
 if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
-
-import anchorScroll from "./scripts/anchorScroll";
-anchorScroll();
-
-import parallax from "./scripts/parallax";
-parallax();
-
-import headerFixation from "./scripts/headerFixation";
-headerFixation();
-
-
